9be2ab68ddc21b3fdca9e8b474cb909aadfdbe69
Duy <dragonslayer110464@gmail.com>
2022-06-02 20:58:31 +0700

    a

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] arg){
         System.out.println("Hello world!");
-        System.out.println("test 1");
+        System.out.println("test 2");
     }
 }
 
